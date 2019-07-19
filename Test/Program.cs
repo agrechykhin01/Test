c635b162bc088846a8bf1799e44a1e7d42e191bf
AG <brownhero@hotmail.com>
2019-07-19 20:45:08 +0300

    test

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,7 +10,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("tessdfdsfsft");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
